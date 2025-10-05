d6085a4f90edba19972bc27a71a09220b423b8b8
Artur Valiullin <ar.valiullin@yandex.ru>
2025-10-05 19:21:51 +0300

    iter1 Обновил cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/arvaliullin/metrics-collection-service/internal/handler/counter"
+	"github.com/arvaliullin/metrics-collection-service/internal/handler/gauge"
+	"github.com/arvaliullin/metrics-collection-service/internal/repository"
+)
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+
+	storage := repository.NewEmptyMemStorage()
+	counterHandler := counter.NewCounterHandler(storage)
+	gaugeHandler := gauge.NewGaugeHandler(storage)
+
+	mux := http.NewServeMux()
+
+	mux.Handle(`POST /update/counter/{id}/{value}`, counterHandler)
+	mux.Handle(`POST /update/gauge/{id}/{value}`, gaugeHandler)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		panic(err)
+	}
 }
